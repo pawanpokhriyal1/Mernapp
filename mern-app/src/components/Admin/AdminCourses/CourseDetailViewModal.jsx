e93c260897847938ef61c64388e6f6e5ed98cec9
pawanpokhriyal1 <pokhriyal.pawan.1407@gmail.com>
2023-05-20 09:46:12 +0530

    Adding video lectures ui functionality changes

--- mern-app/src/components/Admin/AdminCourses/CourseDetailViewModal.jsx
+++ mern-app/src/components/Admin/AdminCourses/CourseDetailViewModal.jsx
@@ -3,17 +3,21 @@ import {
   Button,
   Grid,
   Heading,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalHeader,
   ModalOverlay,
   Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { RiDeleteBin7Fill } from 'react-icons/ri';
+import { fileUploadCss } from '../../Auth/Register';
 
 const CourseDetailViewModal = ({
   courseTitle,
@@ -22,14 +26,39 @@ const CourseDetailViewModal = ({
   onClose,
   deleteButtonHandler,
   addLectureHandler,
-  lectures = [],
+  lectures = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [video, setVideo] = useState('');
+  const [videoPrev, setVideoPrev] = useState('');
+  const changeVideoHandler = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setVideoPrev(reader.result);
+      setVideo(file);
+    };
+  };
+  const onCloseHandler = () => {
+    setVideoPrev('');
+    setTitle('');
+    setVideoPrev('');
+    setDescription('');
+    onClose();
+  };
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size={'full'}>
+    <Modal
+      isOpen={isOpen}
+      onClose={onCloseHandler}
+      size={'4xl'}
+      scrollBehavior="inside"
+    >
       <ModalOverlay>
         <ModalContent>
           <ModalHeader>{courseTitle}</ModalHeader>
-          <ModalCloseButton />
+          <ModalCloseButton onClick={onClose} />
           <ModalBody p="16">
             <Grid templateColumns={['1fr', '3fr 1fr']}>
               <Box px={['0', '16']}>
@@ -37,20 +66,76 @@ const CourseDetailViewModal = ({
                   <Heading children={courseTitle} />
                   <Heading children={`#${id}`} size="sm" opacity={0.4} />
                   <Heading children={'Lectures'} size={'lg'} />
-                  <VideoCard
-                    title={'Mern Intro'}
-                    description={
-                      'This is a intro lecture,where you will know basics of Mern'
+                  {lectures.map((item, index) => (
+                    <VideoCard
+                      title={'Mern Intro'}
+                      description={
+                        'This is a intro lecture,where you will know basics of Mern'
+                      }
+                      num={1 + index}
+                      lectureId={`as+${index}`}
+                      courseId={id + index}
+                      deleteButtonHandler={deleteButtonHandler}
+                    />
+                  ))}
+                </Box>
+                <Box>
+                  <form
+                    onSubmit={e =>
+                      addLectureHandler(e, id, title, description, video)
                     }
-                    num={1}
-                    lectureId={'asasa212'}
-                    courseId={id}
-                    deleteButtonHandler={deleteButtonHandler}
-                  />
+                  >
+                    <VStack spacing={'4'}>
+                      <Heading
+                        children="Add Lecture"
+                        size={'md'}
+                        textTransform={'uppercase'}
+                      />
+                      <Input
+                        focusBorderColor="purple.300"
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                      />
+                      <Input
+                        focusBorderColor="purple.300"
+                        placeholder="Description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                      />
+                      <Input
+                        accept="video/mp4*"
+                        required
+                        id="chooseAvatar"
+                        type="file"
+                        focusBorderColor="purple.300"
+                        css={{
+                          '&::file-selector-button': {
+                            ...fileUploadCss,
+                            color: 'purple',
+                          },
+                        }}
+                        onChange={changeVideoHandler}
+                      />
+                      {videoPrev && (
+                        <video
+                          controlsList="nodownload"
+                          controls
+                          src={videoPrev}
+                        ></video>
+                      )}
+                      <Button w={'full'} colorScheme="purple" type="submit">
+                        Upload
+                      </Button>
+                    </VStack>
+                  </form>
                 </Box>
               </Box>
             </Grid>
           </ModalBody>
+          <ModalFooter>
+            <Button onClick={onCloseHandler}>Close</Button>
+          </ModalFooter>
         </ModalContent>
       </ModalOverlay>
     </Modal>
