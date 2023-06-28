85ba698efb011280a7f9ed7e35082a0681c8f903
pawanpokhriyal1 <pokhriyal.pawan.1407@gmail.com>
2023-06-28 08:43:18 +0530

    file uploading using cloudinary multer

--- mern-app-server/controllers/courseController.js
+++ mern-app-server/controllers/courseController.js
@@ -1,10 +1,11 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { Course } from "../models/Course.js";
+import getDataUri from "../utils/dataUri.js";
 import ErrorHandler from "../utils/errorHandler.js";
-
+import cloudinary from "cloudinary"
 
 export const getAllCourse = catchAsyncError(async (req, res, next) => {
-    const courses = await Course.find().find().select("-lectures");
+    const courses = await Course.find().select("-lectures");
     res.status(200).json({
         success: true,
         courses,
@@ -15,7 +16,10 @@ export const createCourse = catchAsyncError(async (req, res, next) => {
     const { title, description, category, createdBy } = req.body;
     if (!title || !description || !category || !createdBy)
         return next(new ErrorHandler("Please add all fields", 400));
-    // const file = req.file;
+    const file = req.file;
+    const fileUri = getDataUri(file);
+
+    const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
 
     await Course.create({
         title,
@@ -23,13 +27,48 @@ export const createCourse = catchAsyncError(async (req, res, next) => {
         category,
         createdBy,
         poster: {
-            Public_id: "temp",
-            url: "temp",
+            Public_id: mycloud.public_id,
+            url: mycloud.secure_url,
         }
 
     })
     res.status(201).json({
         success: true,
         message: "Course Created successfully.You can add lectures ",
+    });
+})
+
+export const getCourseLectures = catchAsyncError(async (req, res, next) => {
+    const course = await Course.findById(req.params.id)
+    if (!course) return next(new ErrorHandler("Course not Found", 404));
+
+    course.views += 1;
+    await course.save();
+    res.status(200).json({
+        success: true,
+        lectures: course.lectures,
+    });
+})
+
+export const addCourseLectures = catchAsyncError(async (req, res, next) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    //const file=re.file
+
+    const course = await Course.findById(id);
+
+    if (!course) return next(new ErrorHandler("Course not Found", 404));
+    // upload file here
+
+    course.lectures.push({
+        title, description, video: { public_id: "url", url: "url" }
+    })
+
+    course.numOfVideos = course.lectures.length;
+    await course.save();
+    res.status(200).json({
+        success: true,
+        lectures: "lecture added",
     });
 })
--- mern-app-server/routes/courseRoutes.js
+++ mern-app-server/routes/courseRoutes.js
@@ -1,13 +1,20 @@
 import express from "express"
-import { createCourse, getAllCourse } from "../controllers/courseController.js";
+import { addCourseLectures, createCourse, getAllCourse, getCourseLectures } from "../controllers/courseController.js";
+import singleUpload from "../middlewares/multer.js";
 
 const router = express.Router();
 //Get All courses without Lectures
 router.route("/courses").get(getAllCourse)
 // Create new Course -only admin
-router.route("/createcourse").post(createCourse)
+router.route("/createcourse").post(singleUpload, createCourse)
 
 //Add Lecture ,Delete lecture ,Get Course Details
+
+//Get Lectures of particular course
+router.route("/course/:id").get(getCourseLectures)
+
+//Add Lectures to particular Course
+router.route("/course/:id").post(singleUpload, addCourseLectures)
 
 // Delete lectures
 export default router;
